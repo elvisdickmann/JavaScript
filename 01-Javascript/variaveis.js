b14a27b886367ef3d05e10f9a1d5c781f2099c12
elvisdickmann <elvis.dickmann@hotmail.com>
2024-03-23 13:23:42 -0300

    Enviando arquivos

---
+++ 01-Javascript/variaveis.js
@@ -0,0 +1,12 @@
+console.log('Conceito e tipo de Variáveis')
+
+// VARIÁVEIS
+let nome = 'Elvis'
+let numero = 15;
+let verdade = true;
+let salario = 1978.99;
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof verdade);
+console.log(typeof salario);
